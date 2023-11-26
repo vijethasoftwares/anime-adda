c13e17dce60290098133b9fda7bc8d3ddef71688
WebRizen <webrizen@gmail.com>
2023-11-26 11:56:21 +0530

    responsive

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default async function Home() {
   return (
     <>
       <Hero />
-      <section className='container mx-auto px-16 py-2'>
+      <section className='container mx-auto md:px-16 px-4 py-2'>
         <h2 className="text-3xl text-white font-bold mb-10">Explore Anime</h2>
 
         <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
